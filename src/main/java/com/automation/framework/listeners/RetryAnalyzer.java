ac37fc4dbe2e75e6a0e0b4ad0cd6b0ceb56fefea
automate-with-abdul <maniyar.abdulm@gmail.com>
2025-08-27 15:35:17 +0530

    chore: update dependencies and adjust test configurations

--- src/main/java/com/automation/framework/listeners/RetryAnalyzer.java
+++ src/main/java/com/automation/framework/listeners/RetryAnalyzer.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
     private int retryCount = 0;
-    private static final int MAX_RETRY_COUNT = 2;
+    private static final int MAX_RETRY_COUNT = 1;
 
     @Override
     public boolean retry(ITestResult result) {
